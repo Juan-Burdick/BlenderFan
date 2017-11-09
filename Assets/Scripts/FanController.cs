d22e60c676e96a16e1bf2b274752de991fc19bc2
Juan Burdick <burdick@andrews.edu>
2017-11-09 03:31:13 -0500

    progress

---
+++ Assets/Scripts/FanController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour {
+	private Animator anim;
+	private MeshRenderer fanRenderer;
+	public GameObject hideEffect;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		fanRenderer = GetComponent<MeshRenderer>();
+	}
+
+	public void DoSpin() {
+		anim.CrossFade("Spin", 1);
+	}
+
+	public void Hide() {
+		fanRenderer.enabled = false;
+		Instantiate(hideEffect, transform.position, transform.rotation);
+	}
+
+	public void Show() {
+		fanRenderer.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
